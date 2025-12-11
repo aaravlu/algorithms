dfe87f980d7df6abe44a5a64de461b51082fde8e
aaravlu <demolemon@outlook.at>
2025-12-11 16:12:18 +0800

    Update

---
+++ src/bin/148.rs
@@ -0,0 +1,17 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+pub fn sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    todo!()
+}
+
+fn main() {}
