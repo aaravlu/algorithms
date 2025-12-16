2dab4e9c1e2c149b389019c605536fc30f2dab04
aaravlu <demolemon@outlook.at>
2025-12-16 14:12:39 +0800

    Update 543.rs

--- src/bin/543.rs
+++ src/bin/543.rs
@@ -16,24 +16,24 @@ impl TreeNode {
     }
 }
 
-use core::cmp;
 use std::cell::RefCell;
 use std::rc::Rc;
 pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     let mut diameter = 0;
 
-    fn dfs(node: Option<Rc<RefCell<TreeNode>>>, diameter: &mut i32) -> i32 {
-        if let Some(n) = node {
-            let left = dfs(n.borrow().left.clone(), diameter);
-            let right = dfs(n.borrow().right.clone(), diameter);
-            *diameter = cmp::max(*diameter, left + right);
-            cmp::max(left, right) + 1
+    fn recurse(node: Option<Rc<RefCell<TreeNode>>>, diameter: &mut i32) -> i32 {
+        if let Some(node) = node {
+            let node_borrow = node.borrow();
+            let left = recurse(node_borrow.left.clone(), diameter);
+            let right = recurse(node_borrow.right.clone(), diameter);
+            *diameter = core::cmp::max(*diameter, left + right);
+            core::cmp::max(left, right) + 1
         } else {
             0
         }
     }
 
-    dfs(root, &mut diameter);
+    recurse(root, &mut diameter);
     diameter
 }
 
