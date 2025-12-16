2b89e8b8e45117433007f4f2de372a05cc1b6aa7
aaravlu <demolemon@outlook.at>
2025-12-16 13:58:23 +0800

    Update 199.rs

--- src/bin/199.rs
+++ src/bin/199.rs
@@ -43,4 +43,32 @@ pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     res
 }
 
+pub fn _right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut ret = Vec::new();
+
+    if let Some(root) = root {
+        let mut queue = VecDeque::new();
+        queue.push_back(root);
+
+        while !queue.is_empty() {
+            let level_size = queue.len();
+            for i in 0..level_size {
+                let node = queue.pop_front().unwrap();
+                let node_borrow = node.borrow();
+
+                if i == level_size - 1 {
+                    ret.push(node_borrow.val);
+                }
+                if let Some(left) = node_borrow.left.clone() {
+                    queue.push_back(left);
+                }
+                if let Some(right) = node_borrow.right.clone() {
+                    queue.push_back(right);
+                }
+            }
+        }
+    }
+    ret
+}
+
 fn main() {}
