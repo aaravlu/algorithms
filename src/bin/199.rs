64ac30d2ce4c9f7eddb16eb0c35469796e6e279c
aaravlu <demolemon@outlook.at>
2025-12-16 13:58:38 +0800

    Update 199.rs

--- src/bin/199.rs
+++ src/bin/199.rs
@@ -18,32 +18,8 @@ impl TreeNode {
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::rc::Rc;
-pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-    let mut res = Vec::new();
-    if root.is_none() {
-        return res;
-    }
-    let mut queue = VecDeque::new();
-    queue.push_back(root.unwrap());
-    while !queue.is_empty() {
-        let level_size = queue.len();
-        for i in 0..level_size {
-            let node = queue.pop_front().unwrap();
-            if i == level_size - 1 {
-                res.push(node.borrow().val);
-            }
-            if let Some(left) = node.borrow().left.clone() {
-                queue.push_back(left);
-            }
-            if let Some(right) = node.borrow().right.clone() {
-                queue.push_back(right);
-            }
-        }
-    }
-    res
-}
 
-pub fn _right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     let mut ret = Vec::new();
 
     if let Some(root) = root {
