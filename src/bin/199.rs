65a9c4a8b5765abb8b2359ae131b4500c2651c06
aaravlu <demolemon@outlook.at>
2025-12-17 10:16:36 +0800

    Update

--- src/bin/199.rs
+++ src/bin/199.rs
@@ -32,13 +32,23 @@ pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
                 let node = queue.pop_front().unwrap();
                 let node_borrow = node.borrow();
 
-                if i == level_size - 1 {
+                // if i == level_size - 1 {
+                //     ret.push(node_borrow.val);
+                // }
+                // if let Some(left) = node_borrow.left.clone() {
+                //     queue.push_back(left);
+                // }
+                // if let Some(right) = node_borrow.right.clone() {
+                //     queue.push_back(right);
+                // }
+
+                if i == 0 {
                     ret.push(node_borrow.val);
                 }
-                if let Some(left) = node_borrow.left.clone() {
+                if let Some(left) = node_borrow.right.clone() {
                     queue.push_back(left);
                 }
-                if let Some(right) = node_borrow.right.clone() {
+                if let Some(right) = node_borrow.left.clone() {
                     queue.push_back(right);
                 }
             }
--- src/bin/236.rs
+++ src/bin/236.rs
@@ -22,30 +22,33 @@ pub fn lowest_common_ancestor(
     p: Option<Rc<RefCell<TreeNode>>>,
     q: Option<Rc<RefCell<TreeNode>>>,
 ) -> Option<Rc<RefCell<TreeNode>>> {
-    // 如果根节点为空，返回 Nones
-    root.as_ref()?;
+    let p = p.unwrap();
+    let q = q.unwrap();
 
-    // 获取根节点的引用
-    let root_rc = root.unwrap();
+    fn recurse(
+        node: Option<Rc<RefCell<TreeNode>>>,
+        p: Rc<RefCell<TreeNode>>,
+        q: Rc<RefCell<TreeNode>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(node) = node {
+            if Rc::ptr_eq(&node, &p) || Rc::ptr_eq(&node, &q) {
+                return Some(node.clone());
+            }
+            let node_borrow = node.borrow();
+            let left = recurse(node_borrow.left.clone(), p.clone(), q.clone());
+            let right = recurse(node_borrow.right.clone(), p.clone(), q.clone());
 
-    // 如果根节点是 p 或 q 中的任意一个，返回根节点
-    if Rc::ptr_eq(&root_rc, p.as_ref().unwrap()) || Rc::ptr_eq(&root_rc, q.as_ref().unwrap()) {
-        return Some(root_rc);
+            if left.is_some() && right.is_some() {
+                Some(node.clone())
+            } else {
+                left.or(right)
+            }
+        } else {
+            None
+        }
     }
 
-    // 递归查找左子树中的 LCA
-    let left = lowest_common_ancestor(root_rc.borrow().left.clone(), p.clone(), q.clone());
-
-    // 递归查找右子树中的 LCA
-    let right = lowest_common_ancestor(root_rc.borrow().right.clone(), p.clone(), q.clone());
-
-    // 如果左子树和右子树都找到了 LCA，则当前根节点是 LCA
-    if left.is_some() && right.is_some() {
-        return Some(root_rc);
-    }
-
-    // 如果只有一个子树找到了，返回那个结果，否则返回 None
-    left.or(right)
+    recurse(root, p.clone(), q.clone())
 }
 
 fn main() {}
